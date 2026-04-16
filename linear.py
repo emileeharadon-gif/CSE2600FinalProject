777ca27e68be6fb38b507b69caa85506b16f1d04
Emilee Haradon <egh23013@uconn.edu>
2026-04-16 19:15:15 -0400

    train and test

--- linear.py
+++ linear.py
@@ -10,4 +10,13 @@ from ISLP.models import (ModelSpec as MS, summarize)
 Aero = load_data('Aero')
 Weather = load_data('Weather')
 
+Aero.dtypes
+Weather.dtypes
 
+X_train, X_test, y_train, y_test, Train, Test = train_test_split(X, y, Aero, 
+                                                                 test_size = 0.25, 
+                                                                 shuffle = True)
+
+X_train, X_test, y_train, y_test, Train, Test = train_test_split(X, y, Weather, 
+                                                                 test_size = 0.25, 
+                                                                 shuffle = True)
