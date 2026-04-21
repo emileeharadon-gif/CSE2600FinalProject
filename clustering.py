a7e33a4a60e38f334409e617cd1f5f8675076b84
Dung Phan <anhdungphan2006@gmail.com>
2026-04-20 20:08:31 -0400

    clustering

--- clustering.py
+++ clustering.py
@@ -9,8 +9,36 @@ from sklearn.preprocessing import StandardScaler
 
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.metrics import mean_absolute_error
 from scipy.cluster.hierarchy import dendrogram, cut_tree
 from ISLP.cluster import compute_linkage
 
-aero = load_data('Aero')
 weather = load_data('Weather')
+
+feature = [
+    "AirTemp",
+    "Humidity",
+    "Pressure",
+    "Rainfall",
+    "WindSpeed",
+]
+
+X = weather[feature]
+scaler = StandardScaler().set_output(transform="pandas")
+X_scaled = scaler.fit_transform(X)
+
+kmeans = KMeans(n_clusters=4, random_state=42)
+weather['cluster'] = kmeans.fit_predict(X_scaled)
+
+cluster_temp_map = weather.groupby('cluster')['TrackTemp'].mean()
+weather['predicted_TrackTemp'] = weather['cluster'].map(cluster_temp_map)
+
+mae = mean_absolute_error(weather['TrackTemp'], weather['predicted_TrackTemp'])
+print("Mean Absolute Error:", mae)
+
+plt.scatter(weather['AirTemp'], weather['TrackTemp'], c=weather['cluster'])
+plt.xlabel('AirTemp')
+plt.ylabel('TrackTemp')
+plt.title('K-Means Clustering of Weather Data on Track Temperature')
+plt.colorbar(label='Cluster')
+plt.show()
