eac7a47e2a07a635d28f880391f70aef8b72291f
Emilee Haradon <egh23013@uconn.edu>
2026-04-26 19:20:53 -0400

    adding no track id rf

--- rf_no_trackid.py
+++ rf_no_trackid.py
@@ -1,8 +1,7 @@
-#imports, we might not actually use all of these so delete what doesn't get used at the end
 import pandas as pd 
 from sklearn.ensemble import \
      (RandomForestRegressor as RFR)
-from sklearn.model_selection import cross_val_score, GroupKFold
+from sklearn.model_selection import cross_val_score, GroupKFold, train_test_split
 
 
 #read data files
@@ -12,10 +11,6 @@ weather = pd.read_csv("data/Weather.csv")
 weather["Time"] = pd.to_timedelta(weather["Time"]).dt.total_seconds()
 weather = weather.sort_values(by=["Year", "Round Number", "Time"])
 
-weather["TrackTemp_lag1"] = weather.groupby(
-    ["Year", "Round Number"]
-)["TrackTemp"].shift(1)
-
 weather["Time_norm"] = weather.groupby(
     ["Year", "Round Number"]
 )["Time"].transform(lambda x: x / x.max())
@@ -24,15 +19,17 @@ weather = weather.dropna()
 
 races = weather[["Year", "Round Number"]].drop_duplicates()
 split_idx = int(len(races) * 0.8)
+
 train_races = races.iloc[:split_idx]
 test_races  = races.iloc[split_idx:]
+
 train = weather.merge(train_races, on=["Year", "Round Number"])
 test  = weather.merge(test_races,  on=["Year", "Round Number"])
 
-X_train = train.drop(columns=["TrackTemp", "WindDirection"])
+X_train = train.drop(columns=["TrackTemp", "WindDirection", "Round Number"])
 y_train = train["TrackTemp"]
 
-X_test = test.drop(columns=["TrackTemp", "WindDirection"])
+X_test = test.drop(columns=["TrackTemp", "WindDirection", "Round Number"])
 y_test = test["TrackTemp"]
 
 
@@ -66,7 +63,6 @@ rf_weather.fit(X_train, y_train)
 
 #evaluations and feature importance
 #add log loss, oob and whatnot
-rf_weather.score(X_test, y_test)
 error = rf_weather.score(X_test, y_test)
 importances = rf_weather.feature_importances_
 
